df560370bc3f3387b157279548d709e3663bc4eb
Mosudi Moshood <mosudimosudi@gmail.com>
2025-04-26 08:11:24 +0100

    Use the following link during the project https://getbootstrap.com/docs/5.3/examples/

--- main.py
+++ main.py
@@ -5,6 +5,10 @@ from flask import Flask,redirect,url_for,render_template,request
 app=Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def home():
+    task = request.form.get("task")
+    if task:
+        task.append(task)
+        return redirect(url_for("home"))
     if request.method=='POST':
         # Handle POST Request here
         return render_template('index.html')
